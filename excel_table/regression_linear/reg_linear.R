a327dbadc92c7c87160acc569d4323306f7cc909
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-10-30 16:07:06 +0100

    still working

--- excel_table/regression_linear/reg_linear.R
+++ excel_table/regression_linear/reg_linear.R
@@ -1,7 +1,10 @@
 
-table_filename <- 
 
-table = read.table("domestic_1993-2015.txt", header=FALSE, colClasses = "character", comment.char = "")
+table_filename <- 
+table = read.table(table_filename, header=FALSE, colClasses = "character", comment.char = "")
 
 reg_linear <- lm( as.numeric(table[,2]) ~ as.numeric(table[,1]) - 1)
 summary(reg_linear)
+
+
+
