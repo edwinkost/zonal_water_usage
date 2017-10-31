367abc401b68251d8dc7586e3629d640fdf8616e
Edwin Sutanudjaja <edwinhs@cartesius.surfsara.nl>
2017-10-31 02:33:00 +0100

    Add txt table

--- excel_table/regression_linear/reg_linear.R
+++ excel_table/regression_linear/reg_linear.R
@@ -1,10 +1,15 @@
 
 args <- commandArgs()
 table_filename <- args[4]
+
 table = read.table(table_filename, header=FALSE, colClasses = "character", comment.char = "")
 
-reg_linear <- lm( as.numeric(table[,2]) ~ as.numeric(table[,1]) - 1)
+reg_linear_without_intercept <- lm( as.numeric(table[,2]) ~ as.numeric(table[,1]) - 1)
+summary(reg_linear_without_intercept)
+
+reg_linear <- lm( as.numeric(table[,2]) ~ as.numeric(table[,1]))
 summary(reg_linear)
+
 
 
 
